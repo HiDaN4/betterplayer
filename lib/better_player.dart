f19b879faa19a53fe4de30bfabe300f00dd9a4b6
HiDaN4 <hidan446@gmail.com>
2020-08-26 18:46:02 -0700

    - Make VideoPlayerController exportable in the project

--- lib/better_player.dart
+++ lib/better_player.dart
@@ -3,6 +3,7 @@ library better_player;
 export 'src/core/better_player.dart';
 export 'src/controls/better_player_progress_colors.dart';
 export 'src/core/better_player_controller.dart';
+export 'src/video_player/video_player.dart';
 export 'src/configuration/better_player_data_source.dart';
 export 'src/configuration/better_player_data_source_type.dart';
 export 'src/configuration/better_player_configuration.dart';
